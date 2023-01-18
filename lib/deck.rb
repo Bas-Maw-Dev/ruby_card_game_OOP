5c07c67bd72fcb0a5f2b73a88536a06e239a2c3c
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-01-18 15:52:55 +0000

    refactored - less code for the pack of cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,14 +2,11 @@ require_relative './cards'
 
 class Deck
   def initialize
-    cards = [
-      Card.build(:hearts),
-      Card.build(:clubs),
-      Card.build(:diamonds),
-      Card.build(:spades)
-    ].flatten
-# #flatten - https://www.geeksforgeeks.org/ruby-array-class-flatten-function/
-# #shuffle - https://www.geeksforgeeks.org/ruby-array-shuffle-function/
+    cards = %i[hearts clubs diamonds spades].map do |suit|
+      Card.build(suit)
+    end.flatten
+    # #flatten - https://www.geeksforgeeks.org/ruby-array-class-flatten-function/
+    # #shuffle - https://www.geeksforgeeks.org/ruby-array-shuffle-function/
     @cards = cards.shuffle
   end
 
