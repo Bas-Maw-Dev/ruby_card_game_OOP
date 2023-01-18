9d41921e62b8b31f3374721cac4275d02e8da3ac
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-01-18 16:03:54 +0000

    created a constant for the suits

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,11 @@
 require_relative './cards'
 
-class Deck
+class Deck 
+  SUITS = %i[hearts clubs diamonds spades].freeze
+
   def initialize
-    cards = %i[hearts clubs diamonds spades].map do |suit|
-      Card.build(suit)
-    end.flatten
+    cards = SUITS.map { |suit| Card.build(suit) }.flatten
+    
     # #flatten - https://www.geeksforgeeks.org/ruby-array-class-flatten-function/
     # #shuffle - https://www.geeksforgeeks.org/ruby-array-shuffle-function/
     @cards = cards.shuffle
