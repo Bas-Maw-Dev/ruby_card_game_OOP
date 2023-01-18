38314e504269bcca5bde88e05e7d780b58417231
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-01-18 15:42:06 +0000

    refactor

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,13 +2,15 @@ require_relative './cards'
 
 class Deck
   def initialize
-    hearts = Card.build(:hearts)
-    clubs = Card.build(:clubs)
-    diamonds = Card.build(:diamonds)
-    spades = Card.build(:spades)
+    cards = [
+      Card.build(:hearts),
+      Card.build(:clubs),
+      Card.build(:diamonds),
+      Card.build(:spades)
+    ].flatten
 # #flatten - https://www.geeksforgeeks.org/ruby-array-class-flatten-function/
 # #shuffle - https://www.geeksforgeeks.org/ruby-array-shuffle-function/
-    @cards = [hearts, clubs, diamonds, spades].flatten.shuffle
+    @cards = cards.shuffle
   end
 
   def draw
